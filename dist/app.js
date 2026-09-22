5efd71ddebcd768cd921b72bc5569d17e18dc0ad
Punto prototype <prototype@punto.local>
2026-09-22 16:12:38 -0400

    Use working map tiles and refine compact layout

--- dist/app.js
+++ dist/app.js
@@ -24,8 +24,8 @@
   }
 
   function makeTileLayer() {
-    return L.tileLayer('https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      maxZoom: 19, attribution: '© OpenStreetMap contributors · © CARTO'
+    return L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19, attribution: '© OpenStreetMap contributors'
     });
   }
 
